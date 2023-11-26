a6eafe4063b1c51a6bd1705833df9c6fa43b5cb9
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2023-11-26 13:35:05 +0100

    ci: add android test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-//import io.gitlab.arturbosch.detekt.Detekt
-//import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply (false)
@@ -10,17 +7,3 @@ plugins {
     alias(libs.plugins.devtools.ksp) apply (false)
     alias(libs.plugins.detekt) apply (false)
 }
-
-//allprojects {
-//    detekt {
-//        buildUponDefaultConfig = true // preconfigure defaults
-//        allRules = true // activate all available (even unstable) rules.
-//    }
-//
-//    tasks.withType(Detekt).configureEach {
-//        jvmTarget = JavaVersion.VERSION_1_8
-//    }
-//    tasks.withType(DetektCreateBaselineTask).configureEach {
-//        jvmTarget = JavaVersion.VERSION_1_8
-//    }
-//}
