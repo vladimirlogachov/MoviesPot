0c131e67fb56cbcc311cc0d9b92180010115a6cf
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2024-04-28 16:47:05 +0200

    feat: new shared-data module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply (false)
     alias(libs.plugins.android.library) apply (false)
-    alias(libs.plugins.jetbrains.compose) apply (false)
-    alias(libs.plugins.kotlin.multiplatform) apply (false)
-    alias(libs.plugins.kotlin.android) apply (false)
+    alias(libs.plugins.android.application) apply (false)
     alias(libs.plugins.kotlin.jvm) apply (false)
+    alias(libs.plugins.kotlin.android) apply (false)
+    alias(libs.plugins.kotlin.multiplatform) apply (false)
+    alias(libs.plugins.jetbrains.compose) apply (false)
     alias(libs.plugins.devtools.ksp) apply (false)
+    alias(libs.plugins.build.konfig) apply (false)
     alias(libs.plugins.detekt) apply (false)
 }
---
+++ shared-data/build.gradle.kts
@@ -0,0 +1,94 @@
+import com.codingfeline.buildkonfig.compiler.FieldSpec
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.build.konfig)
+    alias(libs.plugins.mokkery)
+}
+
+val secretsPropertiesFile: File = project.file("secrets.properties")
+val secretsProperties = Properties().apply {
+    load(FileInputStream(secretsPropertiesFile))
+}
+
+buildkonfig {
+    packageName = "com.vlohachov.shared.data"
+    objectName = "TmdbConfig"
+
+    val baseUrl = secretsProperties["base.url"] as String
+    val baseImageUrl = secretsProperties["base.image.url"] as String
+    val apiKey = secretsProperties["api.key"] as String
+    val accessToken = secretsProperties["access.token"] as String
+
+    defaultConfigs {
+        buildConfigField(FieldSpec.Type.STRING, "BASE_URL", baseUrl)
+        buildConfigField(FieldSpec.Type.STRING, "BASE_IMAGE_URL", baseImageUrl)
+        buildConfigField(FieldSpec.Type.STRING, "API_KEY", apiKey)
+        buildConfigField(FieldSpec.Type.STRING, "ACCESS_TOKEN", accessToken)
+    }
+}
+
+kotlin {
+    explicitApi()
+    applyDefaultHierarchyTemplate()
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_17.majorVersion
+            }
+        }
+        publishLibraryVariants("release")
+    }
+
+    jvm("desktop")
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { target ->
+        target.binaries.framework {
+            baseName = "shared-data"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlin.corutiens.core)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.turbine)
+            implementation(libs.kotlin.corutiens.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.vlohachov.shared.data"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+}
