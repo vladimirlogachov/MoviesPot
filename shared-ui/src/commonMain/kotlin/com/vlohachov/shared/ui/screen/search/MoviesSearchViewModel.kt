782b4e61277e58a8bb5cc2545c96b460989a9625
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2024-05-12 13:56:27 +0200

    refactor(app): migrate MoviesSearchViewModelTest

--- shared-ui/src/commonMain/kotlin/com/vlohachov/shared/ui/screen/search/MoviesSearchViewModel.kt
+++ shared-ui/src/commonMain/kotlin/com/vlohachov/shared/ui/screen/search/MoviesSearchViewModel.kt
@@ -1,54 +1,41 @@
 package com.vlohachov.shared.ui.screen.search
 
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 
 @Stable
 internal class MoviesSearchViewModel(pager: MoviesSearchPager) : ViewModel() {
 
     private val _search = MutableStateFlow(value = "")
+    private val _error = MutableStateFlow<Throwable?>(value = null)
 
     val search: Flow<String> = _search.onEach(pager::onQuery)
-
+    val error: StateFlow<Throwable?> = _error
     val movies = pager.pagingDataFlow
         .catch { error -> onError(error = error) }
-        .cachedIn(viewModelScope)
-
-    var error by mutableStateOf<Throwable?>(value = null)
-        private set
+        .cachedIn(scope = viewModelScope)
 
     fun onClear() {
-        viewModelScope.launch {
-            _search.value = ""
-        }
+        _search.tryEmit(value = "")
     }
 
     fun onSearch(search: String) {
-        viewModelScope.launch {
-            _search.value = search
-        }
+        _search.tryEmit(value = search)
     }
 
     fun onError(error: Throwable) {
-        viewModelScope.launch {
-            this@MoviesSearchViewModel.error = error
-        }
+        _error.tryEmit(value = error)
     }
 
     fun onErrorConsumed() {
-        viewModelScope.launch {
-            error = null
-        }
+        _error.tryEmit(value = null)
     }
 
 }
