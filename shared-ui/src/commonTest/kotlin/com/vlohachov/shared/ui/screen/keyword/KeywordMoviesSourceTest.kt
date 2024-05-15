a7cdf7c8b62c7fc18f6c467400281d4fcbf87123
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2024-05-15 15:22:17 +0200

    refactor(app): migrate KeywordMoviesSourceTest

---
+++ shared-ui/src/commonTest/kotlin/com/vlohachov/shared/ui/screen/keyword/KeywordMoviesSourceTest.kt
@@ -0,0 +1,85 @@
+package com.vlohachov.shared.ui.screen.keyword
+
+import androidx.paging.PagingSource
+import com.vlohachov.shared.TestPaginatedData
+import com.vlohachov.shared.domain.model.movie.Movie
+import com.vlohachov.shared.domain.repository.DiscoverRepository
+import com.vlohachov.shared.domain.usecase.DiscoverMovies
+import dev.mokkery.answering.returns
+import dev.mokkery.answering.throws
+import dev.mokkery.every
+import dev.mokkery.matcher.any
+import dev.mokkery.mock
+import dev.mokkery.resetAnswers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlin.js.JsName
+import kotlin.test.Test
+import kotlin.test.expect
+
+class KeywordMoviesSourceTest {
+
+    private val repository = mock<DiscoverRepository> {
+        every {
+            discoverMovies(
+                page = any(),
+                year = any(),
+                genres = any(),
+                keywords = any(),
+                language = any()
+            )
+        } returns flowOf(value = TestPaginatedData)
+    }
+
+    private val useCase = DiscoverMovies(repository = repository)
+
+    @Test
+    fun `loading success`() = runTest {
+        val expected = PagingSource.LoadResult.Page<Int, Movie>(
+            data = TestPaginatedData.data,
+            prevKey = null,
+            nextKey = null,
+        )
+        expect(expected = expected) {
+            KeywordMoviesSource(keywordId = 1, useCase = useCase)
+                .load(
+                    PagingSource.LoadParams.Refresh(
+                        key = null,
+                        loadSize = 1,
+                        placeholdersEnabled = false,
+                    )
+                )
+        }
+    }
+
+    @Test
+    @JsName(name = "loading_error")
+    fun `loading error`() = runTest {
+        val exception = IllegalStateException()
+        val expected = PagingSource.LoadResult.Error<Int, Movie>(throwable = exception)
+
+        resetAnswers(repository)
+
+        every {
+            repository.discoverMovies(
+                page = any(),
+                year = any(),
+                genres = any(),
+                keywords = any(),
+                language = any()
+            )
+        } throws exception
+
+        expect(expected = expected) {
+            KeywordMoviesSource(keywordId = 1, useCase = useCase)
+                .load(
+                    PagingSource.LoadParams.Refresh(
+                        key = null,
+                        loadSize = 1,
+                        placeholdersEnabled = false,
+                    )
+                )
+        }
+    }
+
+}
