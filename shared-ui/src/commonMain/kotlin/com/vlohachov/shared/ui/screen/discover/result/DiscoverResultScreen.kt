35cc425189c8dd0af28424528b9300a45ac71ac2
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2024-06-02 16:59:28 +0200

    refactor(app): migrate DiscoverResultScreenTest

--- shared-ui/src/commonMain/kotlin/com/vlohachov/shared/ui/screen/discover/result/DiscoverResultScreen.kt
+++ shared-ui/src/commonMain/kotlin/com/vlohachov/shared/ui/screen/discover/result/DiscoverResultScreen.kt
@@ -1,16 +1,44 @@
 package com.vlohachov.shared.ui.screen.discover.result
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.testTag
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.vlohachov.shared.core.collectAsLazyPagingItems
 import com.vlohachov.shared.domain.model.movie.Movie
+import com.vlohachov.shared.ui.component.bar.AppBar
+import com.vlohachov.shared.ui.component.bar.ErrorBar
+import com.vlohachov.shared.ui.component.bar.ErrorBarDefaults
+import com.vlohachov.shared.ui.component.button.ScrollToTop
+import com.vlohachov.shared.ui.component.movie.MoviesPaginatedGrid
 import com.vlohachov.shared.ui.screen.Screen
 import com.vlohachov.shared.ui.screen.details.MovieDetailsScreen
+import moviespot.shared_ui.generated.resources.Res
+import moviespot.shared_ui.generated.resources.discover_results
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 import org.koin.core.parameter.parametersOf
 
@@ -63,19 +91,71 @@ internal data object DiscoverResultScreen : Screen<DiscoverResultScreen.Params>(
 
 }
 
+private const val VISIBLE_ITEMS_THRESHOLD = 3
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal expect fun DiscoverResult(
+internal fun DiscoverResult(
     year: Int?,
     genres: IntArray?,
     onBack: () -> Unit,
     onMovieDetails: (movie: Movie) -> Unit,
     viewModel: DiscoverResultViewModel = koinInject { parametersOf(year, genres) },
+    snackbarDuration: SnackbarDuration = SnackbarDuration.Short,
     snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
-)
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    val gridState = rememberLazyGridState()
+    val showScrollToTop by remember {
+        derivedStateOf { gridState.firstVisibleItemIndex > VISIBLE_ITEMS_THRESHOLD }
+    }
+    val error by viewModel.error.collectAsState()
 
-internal object DiscoverResultDefaults {
+    ErrorBar(
+        error = error,
+        duration = snackbarDuration,
+        snackbarHostState = snackbarHostState,
+        onDismissed = viewModel::onErrorConsumed,
+    )
 
-    const val ContentLoadingTestTag = "content_loading"
-    const val ContentErrorTestTag = "content_error"
-
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(connection = scrollBehavior.nestedScrollConnection),
+        topBar = {
+            AppBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = stringResource(resource = Res.string.discover_results),
+                scrollBehavior = scrollBehavior,
+                onBackClick = onBack,
+            )
+        },
+        floatingActionButton = {
+            ScrollToTop(
+                modifier = Modifier.navigationBarsPadding(),
+                visible = showScrollToTop,
+                gridState = gridState,
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(
+                modifier = Modifier
+                    .testTag(tag = ErrorBarDefaults.ErrorTestTag)
+                    .navigationBarsPadding(),
+                hostState = snackbarHostState,
+            )
+        },
+        contentWindowInsets = WindowInsets.ime
+    ) { paddingValues ->
+        MoviesPaginatedGrid(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = paddingValues),
+            state = gridState,
+            columns = GridCells.Fixed(count = 3),
+            movies = viewModel.movies.collectAsLazyPagingItems(),
+            onClick = onMovieDetails,
+            onError = viewModel::onError,
+        )
+    }
 }
