5cae4e7cfb9f78d02dae884ad0083cba364fcbc3
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2024-09-21 17:19:43 +0200

    chore: suppress MainViewController name violation

--- shared-presentation/src/iosMain/kotlin/com/vlohachov/shared/presentation/ui/MainViewController.kt
+++ shared-presentation/src/iosMain/kotlin/com/vlohachov/shared/presentation/ui/MainViewController.kt
@@ -3,6 +3,7 @@ package com.vlohachov.shared.presentation.ui
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 
+@Suppress("FunctionName")
 public fun MainViewController(): UIViewController = ComposeUIViewController {
     MoviesPotApp()
 }
