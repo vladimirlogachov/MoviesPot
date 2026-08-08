4fb3823f2a0cca80c509bfb5253b3fc7358108cf
Volodymyr Lohachov <volodymyr.lohachov@trustthq.com>
2026-08-08 11:11:29 +0200

    chore: update build scripts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,9 +24,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
